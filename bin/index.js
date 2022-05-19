7a3dd1337df79f1316828440361651224b1243f4
Nuradil Alymkulov <nurchik17@gmail.com>
2022-05-19 12:06:15 +0600

    Format code

--- bin/index.js
+++ bin/index.js
@@ -1,95 +1,97 @@
 #!/usr/bin/env node
 
-const yargs = require('yargs');
-const fs = require('fs');
-const path = require('path');
+const yargs = require('yargs')
+const fs = require('fs')
+const path = require('path')
 
-const {generateAnnotationsForFileContents} = require('../src');
-const {logger} = require('../src/logger')
+const { generateAnnotationsForFileContents } = require('../src')
+const { logger } = require('../src/logger')
 
-const cmdOptions = yargs
-    .usage("Usage: scribble-generate")
-    .option('t', {
-      'alias': 'targets',
-      'array': true,
-      'demandOption': true,
-      'description': 'targets to automatically annotate',
-      'requiresArg': true,
-    })
-    .argv;
+const cmdOptions = yargs.usage('Usage: scribble-generate').option('t', {
+    alias: 'targets',
+    array: true,
+    demandOption: true,
+    description: 'targets to automatically annotate',
+    requiresArg: true,
+}).argv
 
-function readDirectory(dir){
-  let all_files = [];
-  try {
-    const files = fs.readdirSync(dir);
-    for (let file of files){
-      const file_path = path.resolve(dir, file);
-      if (file.endsWith('.sol')){
-        all_files.push(file_path);
-        continue;
-      }
-      try {
-        const stat = fs.statSync(file_path);
-        if (stat.isDirectory()){
-          all_files = all_files.concat(readDirectory(file_path));
+function readDirectory(dir) {
+    let allFiles = []
+    try {
+        const files = fs.readdirSync(dir)
+        for (const file of files) {
+            const file_path = path.resolve(dir, file)
+            if (file.endsWith('.sol')) {
+                allFiles.push(file_path)
+                continue
+            }
+            try {
+                const stat = fs.statSync(file_path)
+                if (stat.isDirectory()) {
+                    allFiles = allFiles.concat(readDirectory(file_path))
+                }
+            } catch (e) {
+                logger.warn(`${e}. Skipping target`)
+            }
         }
-      } catch (e) {
-        logger.warn(`${e}. Skipping target`);
-      }
+    } catch {
+        logger.warn(
+            `Unable to generate annotations for the directory ${dir}. Skipping target`
+        )
     }
-  } catch {
-    logger.warn(`Unable to generate annotations for the directory ${targetPath}. Skipping target`)
-  }
-  return all_files;
+    return allFiles
 }
 
-function run(options){
-  logger.info("Generating Scribble annotations for fuzz tests")
-  const targets = options["targets"]
-  for (let target of targets){
-    const targetPath = path.resolve(target)
-    try {
-      const stat = fs.statSync(targetPath);
-      let files = [];
-      if (stat.isDirectory()){
-        files = readDirectory(targetPath)
-        if (files.length === 0) {
-          continue;
+function run(options) {
+    logger.info('Generating Scribble annotations for fuzz tests')
+    const { targets } = options
+    for (const target of targets) {
+        const targetPath = path.resolve(target)
+        try {
+            const stat = fs.statSync(targetPath)
+            let files = []
+            if (stat.isDirectory()) {
+                files = readDirectory(targetPath)
+                if (files.length === 0) {
+                    continue
+                }
+            } else {
+                files = [targetPath]
+            }
+            logger.info(
+                `Instrumenting ${files.length} solidity file(s) for target "${targetPath}"`
+            )
+            for (const file of files) {
+                logger.debug(`Generating annotations for "${file}"`)
+                generateAnnotationsForFile(file)
+            }
+        } catch (e) {
+            logger.warn(`${e}. Skipping target`)
         }
-      } else {
-        files = [targetPath];
-      }
-      logger.info(`Instrumenting ${files.length} solidity file(s) for target "${targetPath}"`);
-      for (let file of files){
-        logger.debug(`Generating annotations for "${file}"`);
-        generateAnnotationsForFile(file);
-      }
-    } catch (e) {
-      logger.warn(`${e}. Skipping target`);
     }
-  }
 }
 
 function generateAnnotationsForFile(filename) {
-    var modifiedContents = "";
-    var totalAnnotationsAdded = 0;
+    let modifiedContents = ''
+    let totalAnnotationsAdded = 0
 
-    const data = fs.readFileSync(filename, 'utf8');
+    const data = fs.readFileSync(filename, 'utf8')
 
     // Perform initial parse
-    result = generateAnnotationsForFileContents(data);
-    modifiedContents = result[0];
-    totalAnnotationsAdded = result[1];
+    ;[modifiedContents, totalAnnotationsAdded] =
+        generateAnnotationsForFileContents(data)
 
     if (totalAnnotationsAdded) {
-        let originalName = filename.split('.').slice(0, -1).join('.')
+        const originalName = filename.split('.').slice(0, -1).join('.')
         fs.copyFileSync(filename, `${originalName}.sol.sg_original`)
-        console.log(`Writing annotations for ${filename}`)
+        logger.info(`Writing annotations for ${filename}`)
         fs.writeFile(filename, modifiedContents, (err) => {
-            if (err) {console.error(err);}
-        });
+            if (err) {
+                logger.error(err)
+            }
+        })
     }
-    return totalAnnotationsAdded;
+    return totalAnnotationsAdded
 }
 
-run(cmdOptions);
+run(cmdOptions)
--- src/logger.js
+++ src/logger.js
@@ -1,9 +1,9 @@
-const winston = require('winston');
+const winston = require('winston')
 
 const logger = winston.createLogger({
-  level: process.env.LOG_LEVEL || 'info',
-  format: winston.format.cli(),
-  transports: [new winston.transports.Console()]
+    level: process.env.LOG_LEVEL || 'info',
+    format: winston.format.cli(),
+    transports: [new winston.transports.Console()],
 })
 
-exports.logger = logger;
+exports.logger = logger
